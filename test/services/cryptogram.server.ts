cd5627424a8b8d44fd2b2c799f2ace75d924f7ac
Tomer Aberbach <tomeraberbach@gmail.com>
2024-05-17 00:16:11 -0400

    test: add more assertions

--- test/services/cryptogram.server.ts
+++ test/services/cryptogram.server.ts
@@ -1,11 +1,31 @@
 import { fc, test } from 'tomer'
 import solveCryptogram from '../../src/services/cryptogram.server.ts'
 import { readDictionary } from '../../src/services/dictionary.server.ts'
+import { parseWords } from '../../src/services/words.server.ts'
 
 const dictionary = await readDictionary()
 
 test.prop([fc.stringOf(fc.constantFrom(...dictionary.alphabet, ` `))], {
   numRuns: 10_000,
-})(`solveCryptogram does not crash`, ciphertext => {
-  solveCryptogram({ ciphertext, dictionary, maxSolutionCount: 5 })
+})(`solveCryptogram works`, ciphertext => {
+  const solutions = solveCryptogram({
+    ciphertext,
+    dictionary,
+    maxSolutionCount: 5,
+  })
+
+  expect(solutions.size).toBeGreaterThanOrEqual(0)
+  expect(solutions.size).toBeLessThanOrEqual(5)
+  for (const [plaintext, cipher] of solutions) {
+    for (const word of parseWords(plaintext, dictionary.alphabet)) {
+      expect(dictionary.wordFrequencies.has(word)).toBeTrue()
+    }
+
+    expect(plaintext).toHaveLength(ciphertext.length)
+    for (const [key, value] of cipher) {
+      expect(dictionary.alphabet).toContain(key)
+      expect(dictionary.alphabet).toContain(value)
+    }
+    expect([...new Set(cipher.values())]).toStrictEqual([...cipher.values()])
+  }
 })
