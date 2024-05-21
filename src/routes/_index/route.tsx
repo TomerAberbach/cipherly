6af6610a88cb7b91c85c8b0bf3df6ecc8897107c
Tomer Aberbach <tomeraberbach@gmail.com>
2024-05-20 22:02:46 -0400

    feat: general improvements

--- src/routes/_index/route.tsx
+++ src/routes/_index/route.tsx
@@ -28,24 +28,25 @@ import logoSvgPath from '~/private/images/logo-white.svg'
 
 const IndexPage = () => (
   <>
-    <main className='z-10 flex flex-1 flex-col items-center justify-center gap-10 py-14 sm:py-20'>
-      <Header />
-      <CiphertextForm />
-    </main>
+    <Main />
     <Footer />
-    <img
-      src={logoSvgPath}
-      alt=''
-      className='absolute bottom-0 right-3 w-1/5 min-w-48 max-w-72'
-    />
+    <BackgroundLogo />
   </>
+)
+
+const Main = () => (
+  <main className='z-10 flex flex-1 flex-col items-center justify-center gap-10 py-14 sm:py-20'>
+    <Header />
+    <Solver />
+  </main>
 )
 
 const Header = () => (
   <header className='flex gap-3'>
     <Balancer
       as='h1'
-      ratio={1}
+      // Native `text-balance: wrap;` doesn't resize the container, which
+      // prevents proper centering.
       preferNative={false}
       className='mt-1.5 text-right text-2xl font-medium sm:mt-1 sm:text-3xl'
     >
@@ -55,7 +56,7 @@ const Header = () => (
   </header>
 )
 
-const CiphertextForm = () => {
+const Solver = () => {
   const { submission, solutions } = useActionData<typeof action>() ?? {}
   const [form, fields] = useForm({
     lastResult: submission,
@@ -63,47 +64,47 @@ const CiphertextForm = () => {
       parseWithZod(formData, { schema: formSchema }),
     constraint: getZodConstraint(formSchema),
   })
-  const navigation = useNavigation()
-  const isSubmitting = useSpinDelay(navigation.state === `submitting`)
+  const isSubmitting = useIsSubmitting()
 
   return (
-    <div className='relative flex w-full max-w-prose flex-1'>
-      <Form
-        method='post'
-        {...getFormProps(form)}
-        className='flex w-full flex-1 flex-col items-center gap-4'
-      >
-        <div className='flex w-full flex-1 flex-col gap-1'>
-          <label htmlFor={fields.text.id}>Ciphertext</label>
-          <div className='relative flex flex-1'>
-            <textarea
-              {...getTextareaProps(fields.text)}
-              placeholder='Enter your ciphertext...'
-              className='w-full flex-1 resize-none rounded-md border-0 bg-neutral-50 p-3 text-neutral-900 ring-neutral-500 focus:ring-neutral-500 focus-visible:ring-[3px]'
-            />
-            {solutions ? (
-              <SolutionsView
-                ciphertext={fields.text.value ?? ``}
-                solutions={solutions}
-              />
-            ) : null}
-            {isSubmitting ? <LoadingView /> : null}
-          </div>
+    <Form
+      method='post'
+      {...getFormProps(form)}
+      className='flex w-full max-w-prose flex-1 flex-col items-center gap-4'
+    >
+      <div className='flex w-full flex-1 flex-col gap-1'>
+        <label htmlFor={fields.text.id}>Ciphertext</label>
+        <div className='relative flex flex-1'>
+          <textarea
+            {...getTextareaProps(fields.text)}
+            placeholder='Enter your ciphertext...'
+            className='w-full resize-none rounded-md border-0 bg-neutral-50 p-3 text-neutral-900 ring-neutral-500 focus:ring-neutral-500 focus-visible:ring-[3px]'
+          />
+          {solutions ? (
+            <Solutions ciphertext={fields.text.value!} solutions={solutions} />
+          ) : null}
+          {isSubmitting ? <Loading /> : null}
         </div>
-        <input
-          {...getInputProps(fields.action, { type: `hidden` })}
-          defaultValue={Action.SOLVE}
-        />
-        <button className='focus:none rounded-md bg-gradient-to-br from-neutral-600 to-neutral-700 px-5 py-1.5 font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500'>
-          Solve
-        </button>
-        <div id={fields.text.errorId}>{fields.text.errors}</div>
-      </Form>
-    </div>
+      </div>
+      <input
+        {...getInputProps(fields.action, { type: `hidden` })}
+        defaultValue={Action.SOLVE}
+      />
+      <button className='rounded-md bg-gradient-to-br from-neutral-600 to-neutral-700 px-5 py-1.5 font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 active:from-neutral-700 active:to-neutral-800'>
+        Solve
+      </button>
+      <div id={fields.text.errorId}>{fields.text.errors}</div>
+    </Form>
   )
 }
 
-const LoadingView = () => (
+const useIsSubmitting = () => {
+  const navigation = useNavigation()
+  const isSubmitting = useSpinDelay(navigation.state === `submitting`)
+  return isSubmitting
+}
+
+const Loading = () => (
   <div className='absolute inset-0 flex items-center justify-center'>
     <span role='status' className='text-xl font-medium text-neutral-900'>
       Solving...
@@ -114,7 +115,7 @@ const LoadingView = () => (
   </div>
 )
 
-const SolutionsView = ({
+const Solutions = ({
   ciphertext,
   solutions,
 }: {
@@ -132,106 +133,123 @@ const SolutionsView = ({
     [],
   )
 
-  if (solutions.length === 0) {
-    return <div>Couldn't find a solution!</div>
-  }
-
-  const { plaintext, cipher } = solutions[solutionIndex]!
   return (
     <div className='absolute inset-10 flex flex-col space-y-5 rounded-md border-neutral-900 bg-neutral-900 p-8'>
       <button type='button' onClick={revalidate} className='ml-auto'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          className='h-6 w-6'
-          aria-label='Close'
-        >
-          <path
-            fillRule='evenodd'
-            d='M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z'
-            clipRule='evenodd'
-          />
-        </svg>
+        <CloseIcon />
       </button>
       <div className='flex flex-1 flex-col justify-center space-y-5'>
-        <div className='space-y-5 overflow-auto text-center'>
-          <p className='inline-block text-left'>{ciphertext}</p>
-          <table className='mx-auto w-max table-fixed border text-center'>
-            <thead>
-              <tr>
-                {Object.keys(cipher).map(ciphertextLetter => (
-                  <th
-                    key={ciphertextLetter}
-                    className='w-[2.5ch] border font-normal'
-                  >
-                    {ciphertextLetter}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                {Object.values(cipher).map(plaintextLetter => (
-                  <td key={plaintextLetter} className='border'>
-                    {plaintextLetter}
-                  </td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
-          <p className='inline-block text-left'>{plaintext}</p>
-        </div>
-        <nav className='mx-auto flex w-min items-center gap-1.5'>
-          <button
-            type='button'
-            onClick={decrementSolutionIndex}
-            className={clsx(`h-6 w-6`, solutionIndex === 0 && `invisible`)}
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='currentColor'
-              className='h-6 w-6'
-              aria-label='Previous'
-            >
-              <path
-                fillRule='evenodd'
-                d='M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </button>
-          <div className='w-[3ch] whitespace-nowrap text-center'>
-            {solutionIndex + 1} / {solutions.length}
-          </div>
-          <button
-            type='button'
-            onClick={incrementSolutionIndex}
-            className={clsx(
-              `h-6 w-6`,
-              solutionIndex === solutions.length - 1 && `invisible`,
-            )}
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='currentColor'
-              className='h-6 w-6'
-              aria-label='Next'
-            >
-              <path
-                fillRule='evenodd'
-                d='M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </button>
-        </nav>
+        {solutions.length === 0 ? (
+          <p className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>
+            Couldn't find a solution!
+          </p>
+        ) : (
+          <>
+            <div className='space-y-5 overflow-auto text-center'>
+              <p className='inline-block text-left'>{ciphertext}</p>
+              <CipherTable cipher={solutions[solutionIndex]!.cipher} />
+              <p className='inline-block text-left'>
+                {solutions[solutionIndex]!.plaintext}
+              </p>
+            </div>
+            <nav className='mx-auto flex w-min items-center gap-1.5'>
+              <button
+                type='button'
+                onClick={decrementSolutionIndex}
+                className={clsx(`h-6 w-6`, solutionIndex === 0 && `invisible`)}
+              >
+                <ChevronLeftIcon />
+              </button>
+              <div className='w-[3ch] whitespace-nowrap text-center'>
+                {solutionIndex + 1} / {solutions.length}
+              </div>
+              <button
+                type='button'
+                onClick={incrementSolutionIndex}
+                className={clsx(
+                  `h-6 w-6`,
+                  solutionIndex === solutions.length - 1 && `invisible`,
+                )}
+              >
+                <ChevronRightIcon />
+              </button>
+            </nav>
+          </>
+        )}
       </div>
     </div>
   )
 }
+
+const CloseIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    className='h-6 w-6'
+    aria-label='Close'
+  >
+    <path
+      fillRule='evenodd'
+      d='M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z'
+      clipRule='evenodd'
+    />
+  </svg>
+)
+
+const CipherTable = ({ cipher }: { cipher: Solution[`cipher`] }) => (
+  <table className='mx-auto w-max table-fixed border text-center'>
+    <thead>
+      <tr>
+        {Object.keys(cipher).map(ciphertextLetter => (
+          <th key={ciphertextLetter} className='w-[2.5ch] border font-normal'>
+            {ciphertextLetter}
+          </th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        {Object.values(cipher).map(plaintextLetter => (
+          <td key={plaintextLetter} className='border'>
+            {plaintextLetter}
+          </td>
+        ))}
+      </tr>
+    </tbody>
+  </table>
+)
+const ChevronLeftIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    className='h-6 w-6'
+    aria-label='Previous'
+  >
+    <path
+      fillRule='evenodd'
+      d='M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z'
+      clipRule='evenodd'
+    />
+  </svg>
+)
+
+const ChevronRightIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    className='h-6 w-6'
+    aria-label='Next'
+  >
+    <path
+      fillRule='evenodd'
+      d='M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z'
+      clipRule='evenodd'
+    />
+  </svg>
+)
 
 const Footer = () => (
   <footer className='z-10 pb-6 text-center text-xs leading-5 text-neutral-50 mix-blend-difference'>
@@ -269,6 +287,14 @@ const ExternalLink = ({
   >
     {children}
   </a>
+)
+
+const BackgroundLogo = () => (
+  <img
+    src={logoSvgPath}
+    alt=''
+    className='absolute bottom-0 right-3 w-1/5 min-w-48 max-w-72'
+  />
 )
 
 // TODO: Add all metadata.
